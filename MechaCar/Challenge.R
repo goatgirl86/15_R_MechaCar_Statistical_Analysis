610282da3852e277aecb69f60b4ce612ffddced1
miller.katlin <miller.katlin@gmail.com>
2022-03-29 22:48:03 -0600

    D2 update

--- MechaCar/Challenge.R
+++ MechaCar/Challenge.R
@@ -2,5 +2,20 @@ library(dplyr)
 
 cars <- read.csv("MechaCar_mpg.csv")
 
-lm(cars)
-summary(lm(cars))
+lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle 
+   + ground_clearance + AWD, data=cars)
+
+
+summary(lm(mpg ~ vehicle_length + vehicle_weight + 
+             spoiler_angle + ground_clearance + AWD, data=cars))
+
+
+
+#-------------------
+
+coils <- read.csv("Suspension_Coil.csv")
+
+total_summary <- summarize(coils, mean(PSI),median(PSI),var(PSI),sd(PSI))
+
+lot_summary <- group_by(coils,Manufacturing_Lot)
+lot_summary_df <- summarize(lot_summary, mean(PSI),median(PSI),var(PSI),sd(PSI))
